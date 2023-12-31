e66a039473a5748dba7b476e10878b4427924545
Madiyar Smanov <madiyarsmanov@MacBook-Air-Madiyar.local>
2023-12-31 16:32:20 +0400

    add jwt tokens

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import UserViewSet, UserCustomUpdateDelete
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view()), #получение токена
     path('', include(router.urls)), # все пользователи
     path('user/<int:id>/', UserCustomUpdateDelete.as_view()), # вывод одного пользователя
+    
 ]
